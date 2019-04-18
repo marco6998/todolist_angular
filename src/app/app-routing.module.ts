8331c6c6ed74c06eeacf51eee9e771c77d28412f
alexvanhell <alejandrodnl.cv@hotmail.com>
2019-04-18 15:01:05 -0500

    Rutas principales, agregando bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from 'src/app/login/login.component';
+import { SignupComponent } from 'src/app/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'signup',
+  component: SignupComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
